93b151ef4ae4aa3f29ed31cd81f8fedf66b98fe9
Pandark <apache.kof@gmail.com>
2013-02-21 19:31:09 +0100

    Reads book one paragraph at a time.

---
+++ README.md
@@ -0,0 +1,19 @@
+JavaScript Éloquent
+===================
+
+Attention : ceci est une version en développement qui ne fonctionne pas
+encore !
+
+Ceci est la traduction du code source depuis lequel est généré
+http://fr.eloquentjavascript.net
+
+Le fichier `book` contient la source du vrai livre.
+
+Pour le générer vous-même, vous devez avoir installé
+[node](http://nodejs.org/) et [npm](http://npmjs.org/).
+Une fois cela fait, installez les bibliothèques nécessaires en
+exécutant la commande suivante dans le répertoire racine :
+<pre>npm install</pre>
+Exécutez ensuite la commande
+<pre>node app.js</pre>
+
---
+++ app.js
@@ -0,0 +1,53 @@
+#! /usr/bin/env node
+var argv = require('optimist')
+  .usage('Parse a file to make a book.\nUsage: $0')
+  .options({
+    'input' :
+      { 'alias' : 'i'
+      , 'description' : 'Input file to be parsed'
+      , 'default' : 'book'
+      }
+    , 'output' :
+      { 'alias' : 'o'
+      , 'description' : 'Output directory'
+      , 'default' : 'web'
+      }
+    , 'format' :
+      { 'alias' : 'f'
+      , 'description' : 'Output format (html, epub, latex)'
+      , 'default' : 'html'
+      }
+    , 'delay' :
+      { 'alias' : 'd'
+      , 'description' : 'Time between git pull'
+      }
+    , 'help' :
+      { 'alias' : 'h'
+      , 'description' : 'Displays a helpful message.'
+      }
+  })
+  .argv;
+
+var util = require('util')
+  , Reader = require('reader');
+
+if (argv.h) {
+  util.puts('Don\'t panic!');
+  return;
+}
+
+var reader = new Reader();
+reader.read(argv.input);
+
+reader.on('error', function (err) {
+  util.puts('Error: ' + err);
+});
+
+reader.on('data', function (data) {
+  util.puts('New data!\n');
+});
+
+reader.on('end', function (data) {
+  util.puts('End: ' + data + '\n');
+});
+
